fd7956dcab956ca80a7dbc08e5ff9e7ff46b5c66
alamhamim@pm.me <sarthok.nextt100@gmail.com>
2024-01-12 23:56:20 -0500

    more selector added and test updated

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
-const { expect } = require('chai');
+const {expect} = require('chai');
 const by = require('../lib/By'); // Replace with the actual path to your module
 
-describe('Test Modules', () => {
+describe('Test Library LIB', () => {
     it('should return a valid XPath locator', () => {
         const xpathLocator = by.xpath('//div[@id="example"]');
         expect(xpathLocator).to.be.an('object'); // Check if it's an object
@@ -15,4 +15,24 @@ describe('Test Modules', () => {
         expect(dynamicLocator.using).to.equal('xpath'); // Check if it's an XPath locator
         expect(dynamicLocator.value).to.equal('//div[@id="example"]/h1[text()="Hello, World!"]'); // Check the XPath value
     });
+    it('should handle name locator', () => {
+        const nameLocator = by.name('uniqueName');
+        expect(nameLocator).to.be.an('object');
+        expect(nameLocator.using).to.equal('css selector');
+        expect(nameLocator.value).to.equal('*[name="uniqueName"]');
+    });
+    it('verify id locator',
+        function () {
+            const idLocator = by.id('uniqueID');
+            expect(idLocator).to.be.an('object');
+            expect(idLocator.using).to.equal('css selector');
+            expect(idLocator.value).to.equal('*[id="uniqueID"]');
+        });
+    it('verify css selector',
+        function () {
+            const idLocator = by.css('.someClassName');
+            expect(idLocator).to.be.an('object');
+            expect(idLocator.using).to.equal('css selector');
+            expect(idLocator.value).to.equal('.someClassName');
+        });
 });
