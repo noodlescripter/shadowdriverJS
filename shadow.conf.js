835d6b943eda0ac466259522f7cd55b045542517
noodlescripter <sarthok.nextt100@gmail.com>
2024-09-02 04:02:47 -0400

    version v2 beta release

--- shadow.conf.js
+++ shadow.conf.js
@@ -1,12 +1,22 @@
 module.exports = {
-    browserName: 'chrome',
+    framework: "mocha",
+    ai_res: true,
+    capabilities: {
+        browserName: 'chrome',
+    },
     mochaTimeout: 90000,
+
     reportName: 'report.html',
     baseURL: 'https://google.com/',
     specs: [
         'e2e/sample.spec.js'
     ],
+    suites:{
+
+    },
     onPrepare: () => {
         browser.manage().window().maximize();
+    },
+    before:()  =>{
     }
 };
