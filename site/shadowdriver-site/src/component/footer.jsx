ab7e2214d6ef6fd863603c342bf7a5ba83e7412e
noodlescripter <sarthok.nextt100@gmail.com>
2024-11-27 01:59:10 -0500

    some logo changes to basic

--- site/shadowdriver-site/src/component/footer.jsx
+++ site/shadowdriver-site/src/component/footer.jsx
@@ -1,6 +1,6 @@
 export default function FooterComponent() {
   return (
-    <footer className="bg-dark text-white py-3 text-center sticky-bottom shadow-sm">
+    <footer className="bg-dark text-white text-center sticky-bottom shadow py-3">
       <p>&copy; 2024 ShadowdriverJS. All rights reserved.</p>
       <p>Made with ❤️ by @noodlescripter (Hamim.Alam [Email: hamim.alam.personal@gmail.com])</p>
     </footer>
